5fc7b2d6e199c36a76ce3cceb1c04ac460d80642
Todd Yarberry <todd@coilsteelprocessing.com>
2026-09-08 10:42:30 -0400

    Expose current shipping MMS deployment mode

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get("/api/deploy-status", (_req, res) => {
       supabaseServiceRole: Boolean(process.env.DRIVER_SUPABASE_SERVICE_ROLE_KEY),
       fromEmail: Boolean(process.env.SHIPPING_AUTH_FROM_EMAIL || process.env.PRO_FORMS_FROM_EMAIL)
     },
-    shippingSmsMode: "twilio-two-way-v11-two-way-mms",
+    shippingSmsMode: "twilio-two-way-v12-mms-limits",
     shippingArrivalLogMode: "appointment-aware-v1",
     shippingArrivalEditMode: "name-company-release-v1",
     shippingSmsConfigured: Boolean(
